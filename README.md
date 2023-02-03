0e652c7ceac0e2a12523911e3a0732c2d7f0abdd
lewardo <lewardo23@gmail.com>
2023-02-03 16:59:14 +0000

    removed range

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 # mkcskc
-## c looks good, but it could look better!  
+
 ### current global additions
 + negated conditions with `ifnt` and `whilnt`
-+ ranged loop for for loops, just very useful
 
 ### c-specific additions
-+ void pointer dereferencer to keep all the ugly casting in one place
++ void pointer dereferencing macro
 
 ### cpp-specific additions
-+ renamed `std::unordered_map` to `hashmap`, so much clearer for beginners
-
-> see LICENSE for licensing, basically if things break it wasn't me, but if they don't you mention me ;)
----
-</> with ❤️ by [@lewardo](https://www.github.com/lewardo) 2021
++ type alias `std::unordered_map` to `hashmap`
--- mkcskc.h
+++ mkcskc.h
@@ -1,43 +1,51 @@
 /* 
  * the mkcskc.h header v1.0, making c sexy(er) since 2021
  * 
- * copyright (c) lewardo all rights reserved
+ * copyright (c) lewardo, all rights reserved
  * 
  */
 
 #pragma once
 
 #ifdef __cplusplus
+
 /*
- *  c++ only region
+ *  cpp-specific additions
  */
 
 #include <cstdint>
 #include <unordered_map>
 
 
-// renaming something which should have been renamed long ago
-template <typename a, typename b>
-using hashtable = std::unordered_map<a, b>
+/*
+ *  type alias 
+ */
+template <typename Ta, typename Tb>
+using hash_table = std::unordered_map<Ta, Tb>
 
 #else
+
 /*
- *  c only region
+ *  c-specific additions
  */
 
 #include <stdint.h>
 
-// such a useful way to pass data, but dereferencing is such a hassle... Not Anymore!
-// useage: pv => pointer to dereference; t => type to cast as
+
+/*
+ *  void pointer dereference
+ */
 #define pv_deref(pv, t) ((t) *((t *) (pv)))
 
 
 #endif
 
-// negating a whole condition is so ugly, so you'll only have to see it here from now on!
+/*
+ *  negated if
+ */
 #define ifnt( condition ) if(!( condition ))
-#define whilnt( condition ) while(!( condition ))
 
-// just a _really_ useful macro! a for loop where the variable name passed as n loops from lo to hi
-// useage: for range(i, 0, 10)
-#define range(n, lo, hi) (signed long long int n = lo; n < hi; ++n)
+/*
+ *  negated while
+ */
+#define whilnt( condition ) while(!( condition ))
